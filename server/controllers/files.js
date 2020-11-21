a149a6e19e1a9f3a6b2e415dc87ae8e2dacf46ec
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-22 01:29:18 +0200

    add model for mongoDB & controller, also upload file is now available

--- server/controllers/files.js
+++ server/controllers/files.js
@@ -1,8 +1,50 @@
-export const addFile = (request, response) => {
+import multer from 'multer' // for more info about multer, just check out their docs from here: https://github.com/expressjs/multer
+import path from 'path' // for using path module, check the docs from here: https://nodejs.org/api/path.html
+import fileSchema from '../models/file.js' // file schema
+import { v4 as uuidv4 } from 'uuid' // this is for generating unique id
+
+// use diskStorage to save the files in upload folder in the root of the server
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'uploads/'),
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`
+        cb(null, uniqueName)
+    }
+})
+
+// this below used for uploading files and save it to the storage above
+// again for more info about multer and how it works just check the docs from here: https://github.com/expressjs/multer
+let upload = multer({
+    storage,
+    limits: { fieldSize: 1000000 * 100 }
+}).single('myfile')
+
+export const addFile = (req, res) => {
+    // for now I guess there is no need to use try/catch block, maybe I remove it later
     try {
-        if (!request.file) {
-            return response.json({ error: 'All fields required' })
-        }
+        // upload file
+        upload(req, res, async (error) => {
+            // validate request
+            if (!req.file) {
+                return res.json({ error: 'All fields required' })
+            }
+
+            if (error) {
+                return res.status(500).send({ error: error.message })
+            }
+            // save to database
+            // 1) create file
+            const file = new fileSchema({
+                filename: req.file.filename,
+                uuid: uuidv4(),
+                path: req.file.path,
+                size: req.file.size
+            })
+            // 2) save it to the database
+            const response = await file.save()
+            return res.json({ file: `${process.env.APP_BASE_URL}/files/${response.uuid}` })
+            // for now the file doesn't actualy upload to the database but it store itself in upload folder in the root of the server
+        })
 
     } catch (error) {
         console.log(error)
---
+++ server/models/file.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const schema = new mongoose.Schema({
+    filename: {
+        type: String,
+        required: true
+    },
+    path: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: Number,
+        required: true
+    },
+    uuid: {
+        type: String,
+        required: true
+    },
+    sender: {
+        type: String,
+        required: false
+    },
+    receiver: {
+        type: String,
+        required: false
+    },
+}, { timestamps: true })
+
+const fileSchema = mongoose.model('File', schema)
+
+export default fileSchema
