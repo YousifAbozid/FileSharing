744cf6d359a22ed7d59a56982a21c09f9dc6e991
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-21 23:00:19 +0200

    connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express() // the actual express app.
 require('dotenv').config() // this config to make the backend read the environment variables,
 // make sure to define it before using any environment variables in your app.
+
+// connecting to MongoDb
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => console.log('connected to MongoDB'))
+    .catch((error) => console.log(`something went wrong, can't connect to MongoDB`, error.message))
 
 const PORT = process.env.PORT || 3001
 
