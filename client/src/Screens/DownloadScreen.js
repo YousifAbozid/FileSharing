a2a75366431949ca7c2f85c44f961c62b43b5ce2
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-22 22:24:15 +0200

    download screen now fetches the file info and display it

--- client/src/Screens/DownloadScreen.js
+++ client/src/Screens/DownloadScreen.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../img/logo.png";
 import DownloadContainer from "../components/DownloadContainer";
+import axios from 'axios'
+
+const url = 'http://localhost:3001/files/2617430c-2601-4e9a-9b71-10fb02bd3548'
 
 function DownloadScreen() {
+  const [state, setstate] = useState({})
+  
+  useEffect(() => {
+    const myfun = async () => {
+      const response = await axios.get(url)
+      setstate(response.data)
+    }
+    myfun()
+  }, [])
+
   return (
     <div className="download-screen">
       <img src={logo} alt="logo" className="logo" />
-      <DownloadContainer />
+      <DownloadContainer state={state} />
     </div>
   );
 }
--- client/src/components/DownloadContainer.js
+++ client/src/components/DownloadContainer.js
@@ -1,7 +1,8 @@
 import React from "react";
 import downloadIcon from "../img/download-sd.svg";
 
-function DownloadContainer({ error }) {
+const DownloadContainer = ({ state }) => {
+  console.log(state)
   return (
     <div className="download">
       <img
@@ -9,23 +10,17 @@ function DownloadContainer({ error }) {
         src={downloadIcon}
         alt="inshare-download"
       />
-      {error ? (
-        <h4>{error.message}😏</h4>
-      ) : (
-        <>
-          <h2>Your file is ready to download</h2>
-          <p>Link expires in 24 hours</p>
-          <div className="download__meta">
-            <h4>fileName</h4>
-            <small>fileSize KB</small>
-          </div>
-          <div className="send-btn-container">
-            <a href="#">Download file</a>
-          </div>
-        </>
-      )}
+      <h2>Your file is ready to download</h2>
+      <p>Link expires in 24 hours</p>
+      <div className="download__meta">
+      <h4> {state.filename} </h4>
+      <small>{state.size / 1000} KB</small>
+      </div>
+      <div className="send-btn-container">
+        <a href={`http://localhost:3001/files/download/${state.uuid}`} >Download file</a>
+      </div>
     </div>
-  );
+  )
 }
 
-export default DownloadContainer;
+export default DownloadContainer
